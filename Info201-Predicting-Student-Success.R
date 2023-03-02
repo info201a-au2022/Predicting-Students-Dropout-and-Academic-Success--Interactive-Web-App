d7f85b3e6d3e8ca4c59a6527ce9716fbfea93681
Jack Villagrand <jackvillagrand@Jacks-MacBook-Pro-48.local>
2023-03-01 16:57:18 -0800

    working

--- Info201-Predicting-Student-Success.R
+++ Info201-Predicting-Student-Success.R
@@ -1,18 +1,7 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+read_delim("dataset.csv")
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
     titlePanel("Old Faithful Geyser Data"),
 
     # Sidebar with a slider input for number of bins 
@@ -32,7 +21,6 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
@@ -47,5 +35,4 @@ server <- function(input, output) {
     })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
