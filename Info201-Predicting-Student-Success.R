618f8f80a6da32efb24067cc969b2cc094704b34
damidia <adamidi@uw.edu>
2023-03-06 17:50:35 -0800

    Updated Project Overview Section

--- Info201-Predicting-Student-Success.R
+++ Info201-Predicting-Student-Success.R
@@ -2,8 +2,10 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-data <- read.delim("data/dataset.csv")
-
+data <- read.csv("data/dataset.csv")
+getwd()
+data1 <- data.frame(data)
+names(data1) <- gsub("\\.", "_", names(data1))
 
 ui <- fluidPage(
   
@@ -22,7 +24,7 @@ ui <- fluidPage(
                ),
                tags$h1("Project Overview", style = "font-size: 2em; margin-top: 20px;"),
               
-               tags$h1("What is our data?", style = "font-size: 2em; margin-top: 20px;"),
+               tags$h1("What is our data?", gistyle = "font-size: 2em; margin-top: 20px;"),
                tags$p("The data set we chose to work with was a data set predicting students’ dropping out, and their academic success. This data was collected by four authors, Valentim Realinho, Jorge Machado, Luis Baptista, and Monica V. Martins. We accessed this data through the website Kaggle. For undergraduate students in a variety of fields, this dataset contains information on their socioeconomic status, academic achievement, and demographics. It can be used to examine how various variables may affect academic success and dropout rates. Information such as the application method, course preference, grades, and economic indicators like the unemployment and inflation rates are all included. This dataset, which may be used across disciplines, offers insightful information about the factors that influence students' decision to continue their education or leave it early."),
                tags$h1("Who is our audience?", style = "font-size: 2em; margin-top: 20px;"),
                tags$p("Several different stakeholders, such as academics, educators, managers, and policymakers in the area of higher education, are anticipated to be quite interested in the dataset under review. Particularly those who are eager to learn more about the elements that influence student success and perseverance as well as those who are actively looking for methods to improve students' performance across a variety of academic fields."),
@@ -61,15 +63,24 @@ ui <- fluidPage(
       ),
       
       tabPanel("Economic Factors and Education", 
+               h1("Do economic factors, such as unemployment, inflation, and GDP, of where students live in, affect their dropout, enrollment or graduation?"),
                sidebarLayout(
                  sidebarPanel(
-                   
+                   width = 4,
+                   selectInput("xvar", "X-axis variable", choices = c("Unemployment_rate", "Inflation_rate", "GDP")),
+                   selectInput("yvar", "Y-axis variable", choices = c("Unemployment_rate", "Inflation_rate", "GDP"))
                  ),
                  mainPanel(
-                   
+                   fluidRow(
+                     offset = 2,
+                     width = 5,
+                     plotOutput("scatter_plot", height = "600px", width = "1000px"),
+                     verbatimTextOutput("corr_info"))
                    
                  )
-               )
+               ),
+               h2("Therefore, unemployment rate, inflation rate and GDP have no clear relationship to the academic success of students,
+                as the correlations are all close to 0.")
       ),
       
       tabPanel("Conclusion", 
@@ -81,6 +92,40 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
+  output$target_counts <- renderTable(
+    {
+      table(data1$Target)
+    }
+  )
+  
+  output$corr_info <- renderPrint({
+    # Select the data based on x and y variable selections
+    cor <- data.frame(x = data1[[input$xvar]], y = data1[[input$yvar]], target = data1$Target)
+    
+    # Compute the correlation value
+    corr <- cor(cor$x, cor$y)
+    
+    # Return the correlation information
+    paste("The correlation between", input$xvar, "and", input$yvar, "is", round(corr, 2))
+  })
+  
+  # Function to create scatter plot
+  createScatterPlot <- function(xvar, yvar, data1) {
+    ggplot(data1, aes_string(x = xvar, y = yvar, color = "Target")) + 
+      geom_point(size = 10) + 
+      labs(x = xvar, y = yvar, title = paste0("Scatter plot of ", xvar, " vs. ", yvar), color = "Target") +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5, vjust = 1))
+  }
+  
+  # Scatter plot output
+  scatter_data <- reactive({
+    createScatterPlot(input$xvar, input$yvar, data1)
+  })
+  
+  output$scatter_plot <- renderPlot({
+    scatter_data()
+  })
 }
 
 shinyApp(ui = ui, server = server)
